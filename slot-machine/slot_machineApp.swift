63c5411333154d8312247fc8972e9a5f673eb984
Furkan Ayaz <furkan.yz@outlook.com>
2024-11-30 09:43:09 +0300

    Initial Commit

---
+++ slot-machine/slot_machineApp.swift
@@ -0,0 +1,17 @@
+//
+//  slot_machineApp.swift
+//  slot-machine
+//
+//  Created by FURKAN AYAZ on 30.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct slot_machineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
